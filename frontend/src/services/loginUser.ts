0e436bbcaa381b0bc98c41ef7ef14633d59f9868
MatheusAbriz <matheus.abriz@gmail.com>
2025-09-14 19:09:40 -0300

    :FEAT: FEAT/FIX: Adicionando e dando correções no processo de login de usuario

--- frontend/src/services/loginUser.ts
+++ frontend/src/services/loginUser.ts
@@ -1,8 +1,14 @@
 import axios from "axios";
 
 const loginUser = async (email: string, senha: string) => {
+  const usuario = {
+    email: email,
+    senha: senha
+  }
   const response = await axios.post(
-    `${import.meta.env.VITE_APP_BASE_URL}/usuario/logarUsuario/${email}/${senha}`
+    `${import.meta.env.VITE_APP_BASE_URL}/usuario/logarUsuario`,{
+      usuario
+    }
   );
 
   if (response.status === 400) {
--- frontend/vite-env.d-.ts
+++ frontend/vite-env.d-.ts
@@ -1,8 +1,12 @@
 interface ImportMetaEnv {
-  readonly VITE_YOUR_URL: string;
-  readonly VITE_REALM: string;
-  readonly VITE_CLIENT_ID: string;
- 
+  readonly VITE_APP_BASE_URL: string;
+  readonly VITE_GROQ_API_KEY: string;
+  readonly VITE_API_KEY: string;
+  readonly VITE_AUTH_DOMAIN: string;
+  readonly VITE_PROJECT_ID: string;
+  readonly VITE_STORAGE_BUCKET: string;
+  readonly VITE_MESSASSING_SENDER_ID: string;
+  readonly VITE_APP_ID: string;
 }
 
 interface ImportMeta {
